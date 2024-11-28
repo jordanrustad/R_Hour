55c134752d255ec4aeb850144b192f76ff57c557
jordanrustad <rustad3j@uregina.ca>
2024-11-28 09:23:03 -0600

    small edits while running

--- week 1/scripts/Rtutorial_week1.R
+++ week 1/scripts/Rtutorial_week1.R
@@ -147,18 +147,22 @@ nestdata_filtered_string1<-filter(nestdata,NEST=="N32ALK") # Only keeps records 
 rm(nestdata_filtered_string1)
 
 library(stringr)
-nestdata_ALK<-filter(nestdata,str_detect(NEST,"ALK")) # Only keep observations in the NEST column that contain the string ALK (can't do this with base R)
+nestdata_ALK<-filter(nestdata,str_detect(NEST,"ALK")) 
+# Only keep observations in the NEST column that contain the string ALK (can't do this with base R)
 rm(nestdata_ALK)
 
 --------------------------------------
 
-#4. Let's say I have a variable that is coded as 0 or 1, but it's not actually representing a number/count but rather two groups of data. We need to "tell" R that this is a categorical variable and not a numeric
+#'4. Let's say I have a variable that is coded as 0 or 1, but it's not actually representing a number/count
+# but rather two groups of data. We need to "tell" R that this is a categorical variable and not a numeric
+
 str(nestdata) #Note that NestCode is only 0s or 1s (in actuality this represents whether a site is a nest or a random site)
 nestdata$NestCode<-as.factor(nestdata$NestCode)
 str(nestdata)
 
 #5 What if I had information in another dataset that I want to join to this one
 #Import mergdataset.xlsx
+mergedataset <- read_excel('week 1/data/mergedataset.xlsx')
 str(mergedataset) #Note that the column FIELD is common between the new dataset and the one we've been working with
 mergedataset_new<-merge(nestdata,mergedataset,all.x=TRUE)
 
@@ -168,6 +172,7 @@ str(appenddataset_misscols)
 complete_dataset<-rbind(nestdata,appenddataset_misscols)
 
 #Import appenddataset
+appenddataset <- read_excel('week 1/data/appenddataset.xlsx')
 str(appenddataset)
 complete_dataset<-rbind(nestdata,appenddataset)
 
@@ -178,24 +183,42 @@ write.csv(new_appenddataset2,"XXXXX")
 #TRANSPOSING,MOVING DATA AROUND, SUMMARIZING
 #Import Fulldataset_wide.xlsx
 
-#1 We first want to make the columns CNORT, CSOUT, CWEST, CEAST into two columns. One columns indicating what measurement it is and then a column with the measurement itself (going from "wide" data to "long")
+
+#'1 We first want to make the columns CNORT, CSOUT, CWEST, CEAST into two columns. One columns indicating 
+#'what measurement it is and then a column with the measurement itself (going from "wide" data to "long")
 #Great resource - https://seananderson.ca/2013/10/19/reshape/
+
 #Download package reshape2
+
+install.packages("reshape2")
+library(reshape2)
+
 #Load fulldataset_wide.xlsx
 
-library(reshape2)
+fulldataset_wide <- read_excel('week 1/data/Fulldataset_wide.xlsx')
+
 str(fulldataset_wide)
-longdata<-melt(fulldataset_wide,id=c("YEAR","JULIAN","FIELD","HABITAT","NEST"))
+longdata<-melt(fulldataset_wide,id=c("YEAR","JULIAN","FIELD","HABITAT","NEST")) 
+#the rest of the columns are what get flipped; goes from 5 columns to 2 
+
 #rename the variable and value fields
 names(longdata)[names(longdata)=="variable"]<-"Direction"
 names(longdata)[names(longdata)=="value"]<-"cover"
 
 #And then let's go backward
 widedata<-dcast(longdata, YEAR+JULIAN+FIELD+HABITAT+NEST~Direction,value.var="cover")
+#list variables that remain unchanged and then the ~ for the variable we want wide and which value
 
-#* Be careful with some of these functions and always verify they are doing what you want them to do. Null values, NAs can sometimes royally screw these functions up
+#* Be careful with some of these functions and always verify they are doing what you want them to do. 
+#* Null values, NAs can sometimes royally screw these functions up
+#* especially when flipping from long to wide
 
 #2. Lastly let's do some summarizing of data
 #I want to know the average of CNORT, CSOUT, etc by YEAR and FIELD combination
+
 aggregate_YEARFIELD<-aggregate(x=fulldataset_wide, by=list(fulldataset_wide$YEAR,fulldataset_wide$FIELD), FUN="mean")
+
 aggregate_HABITAT<-aggregate(x=fulldataset_wide,by=list(fulldataset_wide$HABITAT),FUN="mean")
+#big difference between errors and warnings; warnings usually still run function and errors are a critical failure
+
+#Take home message; use the resources; it has all the ways that you can use reshape2 
